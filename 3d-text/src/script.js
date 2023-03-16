747aae6b6341252abc6b6ab3c27268d67eec8af0
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-03-16 16:17:33 -0300

    basic: 3d text lesson - files update

--- 3d-text/src/script.js
+++ 3d-text/src/script.js
@@ -1,4 +1,4 @@
- import * as THREE from 'three'
+import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
@@ -19,20 +19,21 @@ const scene = new THREE.Scene()
 /**
  * Axes helper
  */
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
 
 /**
  * Fonts
  */
-const  fontLoader = new FontLoader()
+const fontLoader = new FontLoader()
 
-fontLoader.load (
+fontLoader.load(
     '/fonts/helvetiker_regular.typeface.json',
     (font) => {
         const textGeometry = new TextGeometry(
@@ -49,10 +50,37 @@ fontLoader.load (
                 bevelSegments: 4
             }
         )
-        const textMaterial = new THREE.MeshBasicMaterial()
-        textMaterial.wireframe = true
-        const text = new THREE.Mesh(textGeometry, textMaterial)
+        // textGeometry.computeBoundingBox()
+        // textGeometry.translate(
+        //    - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+        //    - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+        //    - (textGeometry.boundingBox.max.z - 0.03) * 0.5,
+        // )
+        textGeometry.center()
+
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+        // textMaterial.wireframe = true
+        const text = new THREE.Mesh(textGeometry, material)
         scene.add(text)
+
+        const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
+
+        for (let i = 0; i < 250; i++) {
+
+            const donut = new THREE.Mesh(donutGeometry, material)
+
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+        }
     }
 )
 
@@ -65,8 +93,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -108,8 +135,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
