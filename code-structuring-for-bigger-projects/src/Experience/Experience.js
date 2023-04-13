489c04c3a897803613adf13605b5fad518a0eeaa
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-04-13 19:13:46 -0300

    first part of code structuring class

---
+++ code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -0,0 +1,48 @@
+import * as THREE from "three"
+import Sizes from "./Utils/Sizes"
+import Time from "./Utils/Time"
+import Camera from "./Utils/Camera"
+
+let instance = null
+
+export default class Experience {
+
+    constructor(canvas){
+
+        if(instance){
+            return instance
+        }
+
+        instance = this
+        
+        //Globalacess
+        window.experience = this
+
+        //Options
+        this.canvas = canvas
+        
+        // Setup
+        this.sizes = new Sizes()
+        this.time = new Time()
+        this.scene = new THREE.Scene()
+        this.camera = new Camera(this)
+
+        // Sizes resize event
+        this.sizes.on('resize', () => {
+            this.resize()
+        })
+
+        // Time tick event
+        this.time.on('tick', () => {
+            this.update()
+        })
+    }
+
+    resize(){
+        this.camera.resize()
+    }
+
+    update(){
+        this.camera.update()
+    }
+}
