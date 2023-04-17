9e0c3d311d9a350633fb56ff28b8189fb99bfa8d
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-04-17 18:00:28 -0300

    code structuring for bigger projects lesson - upload files

--- code-structuring-for-bigger-projects/src/Experience/Camera.js
+++ code-structuring-for-bigger-projects/src/Experience/Camera.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 
-import Experience from "../Experience.js";
+import Experience from "./Experience.js";
 
 export default class Camera {
 
